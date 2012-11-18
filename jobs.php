4cb6a5598b4c178c153571424f31c58640140693
robbeh <robbeh84@gmail.com>
2012-11-18 19:40:31 +0000

    test data

--- jobs.php
+++ jobs.php
@@ -12,12 +12,12 @@ $json['1234']['property'] = '15 some street';
 $json['3456']['property'] = '123 fake gardens';
 $json['5678']['property'] = '52 Glashier drive';
 $json['2343']['property'] = '28 x street';
-$json['2343']['property'] = '28 x street';
+$json['23434']['property'] = '22 close';
 
 $json['1234']['contract'] = '1';
 $json['3456']['contract'] = '2';
 $json['5678']['contract'] = '3';
 $json['2343']['contract'] = '4';
-$json['2343']['contract'] = '5';
+$json['23434']['contract'] = '5';
 
 echo json_encode($json);
